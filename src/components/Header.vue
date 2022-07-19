e0dbf8c862ee7889be718782c588df44b7eaeecf
smileHan2333 <1335391958@qq.com>
2022-07-19 18:02:06 +0800

    导航栏默认选中个人记录

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,8 +11,14 @@
       style="display:flex;flex-direction:row;justify-content: center;text-align:center"
     >
       <!-- <el-menu-item index="/home">首页</el-menu-item> -->
-      <el-menu-item index="/records">个人记录</el-menu-item>
-      <el-menu-item index="/news">招聘信息</el-menu-item>
+      <el-menu-item index="/records" title="个人记录将会保存在本地"
+        >个人记录</el-menu-item
+      >
+      <el-menu-item
+        index="/news"
+        title="招聘信息将会上传，与大家共享，同时你也可以看到大家上传的信息"
+        >招聘信息</el-menu-item
+      >
     </el-menu>
   </div>
 </template>
@@ -21,7 +27,7 @@ export default {
   name: "Header",
   data() {
     return {
-      activeIndex: "/home"
+      activeIndex: "/records"
     };
   },
   methods: {
